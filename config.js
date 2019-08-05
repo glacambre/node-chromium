6bc822ad4bf25ad8484f6691a1819cfd4539d18c
glacambre <code@lacamb.re>
2019-08-05 17:45:19 +0200

    Update to latest stable version of chromium (v76.0.3809.87)

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ const path = require('path');
 
 module.exports = {
     BIN_OUT_PATH: path.join(__dirname, 'lib', 'chromium'),
-    ARCHIVE_DOWNLOAD_ATTEMPTS_COUNT: 10
+    ARCHIVE_DOWNLOAD_ATTEMPTS_COUNT: 25
 };
